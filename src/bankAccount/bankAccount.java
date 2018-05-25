766da4b3455ae591659f87d7439f8af7291185ef
PDHS lab <pdhslab@dsusd.k12.ca.us>
2018-05-25 10:10:10 -0700

    Just make it lok better pls

--- src/bankAccount/bankAccount.java
+++ src/bankAccount/bankAccount.java
@@ -12,12 +12,15 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class bankAccount {
 
 	int pinan = 1234;
+	int bal = 0;
+	boolean logged = false;
 	
 	JLabel label1;
 	JLabel label12;
@@ -27,8 +30,8 @@ public class bankAccount {
 	JLabel label4;
 	
 	JTextField text1;
-	JTextField text2;
-	JTextField text3;
+	JTextField deAmount;
+	JTextField withAmount;
 	
 	JButton button1;
 	JButton button2;
@@ -37,12 +40,16 @@ public class bankAccount {
 	JButton withdrawBut;
 	JButton exitBut;
 	JButton balanceBut;
+	JButton backBut;
 	
 	JFrame thisGUI;
 	JPanel panel;
 	JPanel panel2;
 	JPanel panel3;
 	JPanel panel4;
+
+	JOptionPane dialoguePin;
+	JOptionPane dialoguePanel4;
 	
 	
 	
@@ -65,7 +72,6 @@ public class bankAccount {
 		panel4.setLayout(null);
 		
 		
-		
 //		.___  ___.  _______ .__   __.  __    __  
 //		|   \/   | |   ____||  \ |  | |  |  |  | 
 //		|  \  /  | |  |__   |   \|  | |  |  |  | 
@@ -74,17 +80,18 @@ public class bankAccount {
 //		|__|  |__| |_______||__| \__|  \______/  
 		
 		
-		ListenForAboutItem lForAboutItem = new ListenForAboutItem();
+		ListenForHomeItem lForHomeItem = new ListenForHomeItem();
 		ListenForlogOutItem lForLogOutItem = new ListenForlogOutItem();
 		ListenForCopyItem lForCopyItem = new ListenForCopyItem();
 		
 		JMenuBar menuBar = new JMenuBar();
 		
 		JMenu fileMenu = new JMenu("File");
-		JMenuItem AboutMenuItem = new JMenuItem("About", KeyEvent.VK_O);
-		AboutMenuItem.addActionListener(lForAboutItem);
-		fileMenu.add(AboutMenuItem);
+		JMenuItem HomeMenuItem = new JMenuItem("Home", KeyEvent.VK_O);
+		HomeMenuItem.addActionListener(lForHomeItem);
+		fileMenu.add(HomeMenuItem);
 		fileMenu.addSeparator();
+		
 		JMenuItem logOutMenuItem = new JMenuItem("Log Out", KeyEvent.VK_P);
 		logOutMenuItem.addActionListener(lForLogOutItem);
 		fileMenu.add(logOutMenuItem);
@@ -166,9 +173,11 @@ public class bankAccount {
 		panel2.setLayout(null);
 		panel2.setBackground(new Color(86, 222, 190));
 		
+		ListenForPanel2Deposit lForPanel2Deposit = new ListenForPanel2Deposit();
+		ListenForPanel2Withdraw lForPanel2Withdraw = new ListenForPanel2Withdraw();
 		
 		label2 = new JLabel();
-		label2.setText("Account:" + 1234);
+		label2.setText("Account: " + 1234);
 		label2.setHorizontalAlignment(JLabel.CENTER);
 		label2.setBounds(0,0,500,30);
 		label2.setOpaque(true);
@@ -176,7 +185,7 @@ public class bankAccount {
 		label2.setFont(new Font(label2.getName(), Font.BOLD, 24));
 		
 		label21 = new JLabel();
-		label21.setText("Balance:" + 1234);
+		label21.setText("Balance: $" + bal);
 		label21.setHorizontalAlignment(JLabel.CENTER);
 		label21.setBounds(0,30,500,30);
 		label21.setOpaque(true);
@@ -187,11 +196,13 @@ public class bankAccount {
 		depositBut = new JButton();
 		depositBut.setBounds(40, 175, 200, 40);
 		depositBut.setText("Deposit");
+		depositBut.addActionListener(lForPanel2Deposit);
 		panel2.add(depositBut);
 		
 		withdrawBut = new JButton();
 		withdrawBut.setBounds(260, 175, 200, 40);
 		withdrawBut.setText("Withdraw");
+		withdrawBut.addActionListener(lForPanel2Withdraw);
 		panel2.add(withdrawBut);
 		
 		
@@ -210,6 +221,11 @@ public class bankAccount {
 		panel3.setLayout(null);
 		panel3.setBackground(new Color(86, 222, 190));
 		
+		ListenForPanel3text lForPanel3Text = new ListenForPanel3text();
+		ListenForPanel3button lForPanel3button = new ListenForPanel3button();
+		ListenForBackBut lForBackButton = new ListenForBackBut();
+
+		
 		label3 = new JLabel();
 		label3.setText("Withdraw");
 		label3.setHorizontalAlignment(JLabel.CENTER);
@@ -219,16 +235,23 @@ public class bankAccount {
 		label3.setFont(new Font(label3.getName(), Font.BOLD, 24));
 		
 		button2 = new JButton();
-		button2.setBounds(40, 175, 200, 40);
+		button2.setBounds(160, 200, 200, 40);
 		button2.setText("Enter");
+		button2.addActionListener(lForPanel3button);
 		panel3.add(button2);
 		
-		text2 = new JTextField(20);
-		text2.setBounds(220,130, 120, 40);
-		panel3.add(text2);
+		backBut = new JButton();
+		backBut.setBounds(5, 5, 80, 40);
+		backBut.setText("Back");
+		backBut.addActionListener(lForBackButton);
+		panel3.add(backBut);
 		
-		panel3.add(label21);
-		panel3.add(label2);
+		deAmount = new JTextField(20);
+		deAmount.setBounds(160,130, 200, 40);
+		deAmount.addActionListener(lForPanel3Text);
+		panel3.add(deAmount);
+		
+		
 		panel3.add(label3);
 		
 		
@@ -245,6 +268,10 @@ public class bankAccount {
 		panel4.setLayout(null);
 		panel4.setBackground(new Color(86, 222, 190));
 		
+		ListenForPanel4text lForPanel4Text = new ListenForPanel4text();
+		ListenForPanel4button lForPanel4button = new ListenForPanel4button();
+		
+		
 		label4 = new JLabel();
 		label4.setText("Withdraw");
 		label4.setHorizontalAlignment(JLabel.CENTER);
@@ -254,16 +281,22 @@ public class bankAccount {
 		label4.setFont(new Font(label4.getName(), Font.BOLD, 24));
 		
 		button3 = new JButton();
-		button3.setBounds(40, 175, 200, 40);
+		button3.setBounds(160, 200, 200, 40);
 		button3.setText("Enter");
+		button3.addActionListener(lForPanel4button);
 		panel4.add(button3);
 		
-		text3 = new JTextField(20);
-		text3.setBounds(220,130, 120, 40);
-		panel4.add(text3);
+		withAmount = new JTextField(20);
+		withAmount.setBounds(160,130, 200, 40);
+		withAmount.addActionListener(lForPanel4button);
+		panel4.add(withAmount);
 		
-		panel4.add(label21);
-		panel4.add(label2);
+		backBut = new JButton();
+		backBut.setBounds(5, 5, 80, 40);
+		backBut.setText("Back");
+		backBut.addActionListener(lForBackButton);
+		panel4.add(backBut);
+		
 		panel4.add(label3);
 		
 		
@@ -279,12 +312,30 @@ public class bankAccount {
 	
 	
 	
-	private class ListenForAboutItem implements ActionListener{
+	
+	
+//	.___  ___.  _______ .__   __.  __    __  
+//	|   \/   | |   ____||  \ |  | |  |  |  | 
+//	|  \  /  | |  |__   |   \|  | |  |  |  | 
+//	|  |\/|  | |   __|  |  . `  | |  |  |  | 
+//	|  |  |  | |  |____ |  |\   | |  `--'  | 
+//	|__|  |__| |_______||__| \__|  \______/  
+	
+	
+	private class ListenForHomeItem implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			System.out.println("Clicked About Option");
+			System.out.println("Clicked Home Option");
+			if (logged) {
+				panel2.add(label21);
+				panel2.add(label2);
+				thisGUI.setContentPane(panel2);
+				thisGUI.invalidate();
+				thisGUI.validate();
+			}
+			
 		}
 		
 	}
@@ -295,6 +346,9 @@ public class bankAccount {
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			System.out.println("Clicked Log Out Option");
+			logged = false;
+			panel.add(label1);
+			panel.add(label12);
 			thisGUI.setContentPane(panel);
 			thisGUI.invalidate();
 			thisGUI.validate();
@@ -314,12 +368,21 @@ public class bankAccount {
 		
 	}
 	
+	
+//	.______      ___      .__   __.  _______  __          __  
+//	|   _  \    /   \     |  \ |  | |   ____||  |        /_ | 
+//	|  |_)  |  /  ^  \    |   \|  | |  |__   |  |         | | 
+//	|   ___/  /  /_\  \   |  . `  | |   __|  |  |         | | 
+//	|  |     /  _____  \  |  |\   | |  |____ |  `----.    | | 
+//	| _|    /__/     \__\ |__| \__| |_______||_______|    |_| 
+	
 	private class ListenForPanel1Text implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			System.out.println("Panel1 Text");
+			panel1Code();
 		}
 		
 	}
@@ -329,15 +392,176 @@ public class bankAccount {
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			System.out.println("Panel1 Button");
+			panel1Code();
 		}
 		
 	}
 	private void panel1Code() {
 		String txt = text1.getText();
-		txt = txt.trim();
+		txt.trim();
+		if (Integer.parseInt(txt) == pinan) {
+			logged = true;
+			thisGUI.setContentPane(panel2);
+			thisGUI.invalidate();
+			thisGUI.validate();
+		}
+		else {
+			dialoguePin.showMessageDialog(panel, "What are you stupid or something");;
+			System.out.println(txt);
+			System.out.println(pinan);
+		}
 		
-		text1.setText(null);
+	}
+	
+	
+//	.______      ___      .__   __.  _______  __          ___   
+//	|   _  \    /   \     |  \ |  | |   ____||  |        |__ \  
+//	|  |_)  |  /  ^  \    |   \|  | |  |__   |  |           ) | 
+//	|   ___/  /  /_\  \   |  . `  | |   __|  |  |          / /  
+//	|  |     /  _____  \  |  |\   | |  |____ |  `----.    / /_  
+//	| _|    /__/     \__\ |__| \__| |_______||_______|   |____| 
+//	   
+	
+	private class ListenForPanel2Deposit implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Panel2 Deposit");
+			panel3.add(label21);
+			panel3.add(label2);
+			thisGUI.setContentPane(panel3);
+			thisGUI.invalidate();
+			thisGUI.validate();
+			
+		}
+		
+	}
+	private class ListenForPanel2Withdraw implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Panel2 WithDraw");
+			panel4.add(label21);
+			panel4.add(label2);
+			thisGUI.setContentPane(panel4);
+			thisGUI.invalidate();
+			thisGUI.validate();
+			
+		}
+		
+	}
+	
+	//.______      ___      .__   __.  _______  __          ____   
+	//|   _  \    /   \     |  \ |  | |   ____||  |        |___ \  
+	//|  |_)  |  /  ^  \    |   \|  | |  |__   |  |          __) | 
+	//|   ___/  /  /_\  \   |  . `  | |   __|  |  |         |__ <  
+	//|  |     /  _____  \  |  |\   | |  |____ |  `----.    ___) | 
+	//| _|    /__/     \__\ |__| \__| |_______||_______|   |____/  
+
+		
+	
+	private class ListenForPanel3text implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Panel3 text");
+			panel3Code();
+			
+		}
+		
+	}
+	private class ListenForPanel3button implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Panel3 button");
+			panel3Code();
+			
+		}
+		
+	}
+	private class ListenForBackBut implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Back Button");
+				panel2.add(label21);
+				panel2.add(label2);
+				thisGUI.setContentPane(panel2);
+				thisGUI.invalidate();
+				thisGUI.validate();
+		}
 		
 	}
 
+	
+	private void panel3Code() {
+		String txt = deAmount.getText();
+		bal += Integer.parseInt(txt);
+		label21.setText("Balance: $" + bal);
+	}
+	
+	
+	
+//	.______      ___      .__   __.  _______  __          _  _    
+//	|   _  \    /   \     |  \ |  | |   ____||  |        | || |   
+//	|  |_)  |  /  ^  \    |   \|  | |  |__   |  |        | || |_  
+//	|   ___/  /  /_\  \   |  . `  | |   __|  |  |        |__   _| 
+//	|  |     /  _____  \  |  |\   | |  |____ |  `----.      | |   
+//	| _|    /__/     \__\ |__| \__| |_______||_______|      |_|   
+
+	
+	
+	private class ListenForPanel4text implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Panel4 text");
+			panel4Code();
+			
+		}
+		
+	}
+	private class ListenForPanel4button implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Panel4 button");
+			panel4Code();
+			
+		}
+		
+	}
+	private void panel4Code() {
+		String txt = withAmount.getText();
+		if (bal - Integer.parseInt(txt) < 0) {
+			dialoguePanel4.showMessageDialog(panel4, "You can't withdraw that amount, you are too poor!!");;
+		}
+		//else if (//add way to check if string){
+			
+		//}
+		else {
+			bal -= Integer.parseInt(txt);
+		}
+		label21.setText("Balance: $" + bal);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
+
